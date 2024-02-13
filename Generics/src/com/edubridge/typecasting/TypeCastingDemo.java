a71e5a0baf6a6140a8e4f6eaa7c8d342e0e51f12
shukl <shukl@DESKTOP-8BCVFBS>
2024-02-13 13:39:43 +0530

    t

---
+++ Generics/src/com/edubridge/typecasting/TypeCastingDemo.java
@@ -0,0 +1,20 @@
+package com.edubridge.typecasting;
+
+public class TypeCastingDemo {
+
+	public static void main(String[] args) {
+		//implicit or widening typecasting
+		//smaller to larger data type size
+		char c='b';
+		int num=c;
+		System.out.println(num);
+		
+		//explicit or narrowing typecasting
+		//larger to9 smaller data type size
+		int num1=20;
+		char x=(char) num1;
+		System.out.println(x);
+
+	}
+
+}
