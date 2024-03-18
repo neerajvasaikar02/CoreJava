ec812909f30acdff8aca1fced506ea81dfe51c9e
shukl <shukl@DESKTOP-8BCVFBS>
2024-03-18 13:32:57 +0530

    s

---
+++ JDBCSpringDemo/src/com/eb/javatpoint/EmployeeMapper.java
@@ -0,0 +1,22 @@
+package com.eb.javatpoint;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class EmployeeMapper implements RowMapper<Employee>{
+
+	@Override
+	public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+		Employee e1=new Employee();
+		e1.setId(rs.getInt("id"));
+		e1.setName(rs.getString("name"));
+		e1.setSalary(rs.getFloat("salary"));
+		return e1;
+	}
+
+}
+
+		
---
+++ JDBCSpringDemo/src/com/eb/javatpoint/Test1.java
@@ -0,0 +1,10 @@
+package com.eb.javatpoint;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
